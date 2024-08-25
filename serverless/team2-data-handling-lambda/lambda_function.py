931f8b5c567b8c6af41656b1b656b42fce7cbb54
EC2 Default User <ec2-user@ip-172-31-45-187.ap-south-1.compute.internal>
2024-08-25 07:26:04 +0000

    v1.6

--- serverless/team2-data-handling-lambda/lambda_function.py
+++ serverless/team2-data-handling-lambda/lambda_function.py
@@ -186,6 +186,7 @@ def lambda_handler(event, context):
     return {
         'statusCode': 200,
         'body': json.dumps({
+            'success' : True,
             'message': 'Data successfully forwarded to the next Lambda function'
         })
     }
